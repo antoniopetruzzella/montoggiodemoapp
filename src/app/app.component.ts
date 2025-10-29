6cd99ae0c7ba52e82d57509a6c566077af609737
antoniopetruzzella <a.petruzzella71@gmail.com>
2025-10-29 17:07:34 +0100

    aggiunta funzione che autorefresha la app quando modificata in struttura, senza scaricare e ricaricare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PushService } from './services/push.service';
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,9 +16,13 @@ export class AppComponent {
 
   //https://ionic.io/ionicons
   //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor(private router: Router, private pushService: PushService) {
+  constructor(private router: Router, private pushService: PushService,private updates: SwUpdate) {
     console.log('AppComponent constructor chiamato');
      pushService.initialize();
-     
+   this.updates.versionUpdates.subscribe(event => {
+    if (event.type === 'VERSION_READY') {
+      this.updates.activateUpdate().then(() => document.location.reload());
+    }
+  });
   }
 }
